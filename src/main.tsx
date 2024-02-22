7e47df7c89e22b80537e13c764866b84a8c49f5a
Teetow <teetow@gmail.com>
2024-02-23 00:03:19 +0100

    chore: fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route
-          path="/"
+          path="/lab/"
           element={
             <>
               <Cover pages={pages} />
